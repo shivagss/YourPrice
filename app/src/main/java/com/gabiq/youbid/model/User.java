751efcb889e90ec47ed0a1591451481e68fac0bd
Gabi Zuniga <gabiz@alignd.io>
2014-10-15 00:08:36 -0700

    Added favorites to user model

--- app/src/main/java/com/gabiq/youbid/model/User.java
+++ app/src/main/java/com/gabiq/youbid/model/User.java
@@ -1,10 +1,15 @@
 package com.gabiq.youbid.model;
 
+import com.parse.FindCallback;
 import com.parse.ParseClassName;
 import com.parse.ParseFile;
 import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+
+import java.text.ParseException;
+import java.util.List;
 
 @ParseClassName("User")
 public class User extends ParseUser {
@@ -31,5 +36,23 @@ public class User extends ParseUser {
 
     public void setLocation(ParseGeoPoint location) {
         put("location", location);
+    }
+
+    // Favorites accessors
+    public void addToFavorites(Item item) {
+        ParseRelation<ParseObject> relation = getRelation("favorite");
+        relation.add(item);
+        saveInBackground();
+    }
+
+    public void removeFromFavorites(Item item) {
+        ParseRelation<ParseObject> relation = getRelation("favorite");
+        relation.remove(item);
+        saveInBackground();
+    }
+
+    public void getFavorites(FindCallback<ParseObject> findCallback) {
+        ParseRelation<ParseObject> relation = getRelation("favorite");
+        relation.getQuery().findInBackground(findCallback);
     }
 }
