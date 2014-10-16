783f79706c4ec4fcc7dc40fcbf393ac1b7b45b52
sreesharp <sreejumon@gmail.com>
2014-10-16 14:34:08 -0700

    Updating the Item details screen with "add comment" element

--- app/src/main/java/com/gabiq/youbid/activity/DetailsActivity.java
+++ app/src/main/java/com/gabiq/youbid/activity/DetailsActivity.java
@@ -5,6 +5,7 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 import com.gabiq.youbid.R;
 import com.gabiq.youbid.fragment.DetailsFragment;
@@ -29,6 +30,8 @@ public class DetailsActivity extends Activity {
             ft.replace(R.id.container, detailsFragment);
             ft.commit();
         }
+
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
 
