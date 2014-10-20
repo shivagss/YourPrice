95f6bebe5dcb86a5e0fb3e83107a65ace515b79e
Gabi Zuniga <gabiz@alignd.io>
2014-10-19 19:45:37 -0700

    Added push notifications for seller/buyer private messages

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -1,3 +1,67 @@
+Parse.Cloud.afterSave("Message", function(request) {
+  var message = request.object;
+  var bid = message.get("bid");
+  var item = message.get("item");
+  var body = message.get("body");
+  var senderId = message.get("sender").id;
+
+  item.fetch({
+    success: function(updatedItem) {
+
+      bid.fetch({
+        success: function(updatedBid) {
+          var receiverId;
+          // find who we need to send the notification to
+          var sellerId = updatedItem.get("createdBy").id;
+          var buyerId = updatedBid.get("createdBy").id;
+          if (senderId == sellerId) {
+            // send to buyer
+            receiverId = buyerId;
+          }
+          if (senderId == buyerId) {
+            // send to seller
+            receiverId = sellerId;
+          }
+
+          var query = new Parse.Query(Parse.Installation);
+          query.equalTo('userId', receiverId);
+
+          Parse.Push.send({
+            where: query,
+            data: {
+              alert: body,
+              type: "message",
+              bidId: bid.id,
+              itemId: item.id,
+              senderId: senderId
+            }
+          }, {
+            success: function() {
+              // Push was successful
+              console.log("Sent bid push notification to " + seller.id + " successfully");
+            },
+            error: function(error) {
+              // Handle error
+              console.log("Error sending bid push notification to " + seller.id);
+            }
+          });
+
+
+        },
+        error: function(myObject, error) {
+            console.error("bid object not found");
+        }
+      });
+
+    },
+    error: function(myObject, error) {
+        console.error("item object not found");
+    }
+  });
+
+});
+
+
 
 Parse.Cloud.afterSave("Bid", function(request) {
   var bid = request.object;
