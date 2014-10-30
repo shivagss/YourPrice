173317df7b289a9324052aac73a44ee1b1f4a2c3
Shiva Garlapati <shivag@lab126.com>
2014-10-29 21:15:54 -0700

    Layout fixes to Profile/Following/Followers users screen

--- app/src/main/java/com/gabiq/youbid/fragment/ParseUsersListFragment.java
+++ app/src/main/java/com/gabiq/youbid/fragment/ParseUsersListFragment.java
@@ -1,13 +1,19 @@
 package com.gabiq.youbid.fragment;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.app.ListFragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
+import com.gabiq.youbid.R;
 import com.gabiq.youbid.adapter.UsersListAdapter;
 import com.gabiq.youbid.model.Followers;
 import com.parse.FindCallback;
@@ -18,10 +24,15 @@ import com.parse.ParseUser;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ParseUsersListFragment extends ListFragment {
+public class ParseUsersListFragment extends Fragment {
 
     private OnSelectUserListener mListener;
     protected ArrayList<ParseUser> mUsers = new ArrayList<ParseUser>();
+    private ListView lvUsers;
+    private UsersListAdapter mUsersAdapter;
+    private ProgressBar progressBar;
+    private RelativeLayout emptySection;
+    private SwipeRefreshLayout swipeContainer;
 
     public static ParseUsersListFragment newInstance() {
         ParseUsersListFragment fragment = new ParseUsersListFragment();
@@ -51,12 +62,26 @@ public class ParseUsersListFragment extends ListFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v =super.onCreateView(inflater, container, savedInstanceState);
-        return v;
+
+        View view = inflater.inflate(R.layout.fragment_users, container, false);
+
+        lvUsers = (ListView) view.findViewById(R.id.lvUsers);
+        emptySection = (RelativeLayout) view.findViewById(R.id.emptySection);
+        lvUsers.setEmptyView(emptySection);
+        lvUsers.setDivider(getActivity().getResources().getDrawable(android.R.color.transparent));
+
+        mUsersAdapter = new UsersListAdapter(getActivity(), mUsers);
+        lvUsers.setAdapter(mUsersAdapter);
+
+        emptySection.setVisibility(View.GONE);
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        getActivity().setTitle(getTitle());
+
+        setupSwipeContainer(view);
+        return view;
     }
 
     public void updateUI(){
-        getListView().setDivider(getActivity().getResources().getDrawable(android.R.color.transparent));
         ParseQuery<Followers> query = getFollowUsersParseQuery(mListener.getUser());
         query.include("follower");
         query.include("following");
@@ -64,8 +89,11 @@ public class ParseUsersListFragment extends ListFragment {
             query.findInBackground(new FindCallback<Followers>() {
                 @Override
                 public void done(List<Followers> followersList, ParseException e) {
-                    mUsers = loadData(followersList);
-                    setListAdapter(new UsersListAdapter(getActivity(), mUsers));
+                    mUsers.clear();
+                    mUsers.addAll(loadData(followersList));
+                    mUsersAdapter.notifyDataSetChanged();
+                    progressBar.setVisibility(View.GONE);
+//                    emptySection.setVisibility(View.VISIBLE);
                 }
             });
         }
@@ -111,6 +139,24 @@ public class ParseUsersListFragment extends ListFragment {
 
     public interface OnSelectUserListener {
         public ParseUser getUser();
+    }
+
+    private void setupSwipeContainer(View view) {
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.commentListSwipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                updateUI();
+                swipeContainer.setRefreshing(false);
+            }
+        });
+        swipeContainer.setColorScheme(R.color.refreshColor1,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+
     }
 
 }
